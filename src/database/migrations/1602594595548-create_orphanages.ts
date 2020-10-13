2075bec0da4450c69cb26204de3cab9a28d02d04
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-10-13 15:38:11 -0300

    routes, controller and model

--- src/database/migrations/1602594595548-create_orphanages.ts
+++ src/database/migrations/1602594595548-create_orphanages.ts
@@ -39,6 +39,10 @@ export class createOrphanages1602594595548 implements MigrationInterface {
           type: 'text'
         },
         {
+          name: 'opening_hours',
+          type: 'varchar'
+        },
+        {
           name: 'open_on_weekends',
           type: 'boolean',
           default: false
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import './database/connection';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
-
-app.get('/users', (req, res) => res.send({ message: "Hello Strange", status: 200 }));
